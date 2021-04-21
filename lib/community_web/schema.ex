87fe41752571065c887d196989e94d8c0601427c
Ryan <me@ryan.moe>
2021-04-21 23:36:08 +1000

    Learning elixir

---
+++ lib/community_web/schema.ex
@@ -0,0 +1,19 @@
+defmodule CommunityWeb.Schema do
+    use Absinthe.Schema # imports
+
+    alias CommunityWeb.NewsResolver
+
+    object :link do
+        field :id, non_null(:id)
+        field :url, non_null(:string)
+        field :description, non_null(:string)
+    end
+
+    query do
+        # Query entry into app
+        @desc "Get all links"
+        field :all_links, non_null(list_of(non_null(:link))) do
+            resolve(&NewsResolver.all_links/3)
+        end
+    end
+end
